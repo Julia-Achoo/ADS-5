f1872937dfa9604c10253cb6bb8ee7210e6e863d
Юлия <160242381+Julia-Achoo@users.noreply.github.com>
2024-04-13 19:59:21 +0300

    1.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,38 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+    T arr[size];
+    int f;
+
+public:
+    TStack(): f(-1) {}
+    
+    void push(T value) {
+        if (!isFull())
+            arr[++f] = value;
+        else
+            throw std::string("total");
+    }
+
+    T get() const {
+        return arr[f];
+    }
+
+    T pop() {
+        if (isEmpty())
+            throw std::string("null");
+        else
+            return arr[f--];
+    }
+
+    bool isEmpty() const {
+        return f == -1;
+    }
+
+    bool isFull() const {
+        return f == size - 1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
